7b798928830462b89ccf9c1a4f051442f582e74c
JoseMiguelBG <jbermejodc@alumnos.unex.es>
2021-03-26 12:01:49 +0100

    clean code

--- vehicle_get_location.py
+++ vehicle_get_location.py
@@ -79,48 +79,7 @@ try:
     client.set_timeout(2.0)
 
     world = client.get_world()
-    blueprint_library = world.get_blueprint_library()
-    map = world.get_map()
 
-    generados = map.generate_waypoints(15)
-
-    outputfileXY = open("outFile_waypointsXY.txt", "w")
-    outputfileLatLon = open("outFile_waypointsLatLon.txt", "w")
-    
-    x = []
-    y = []
-    filtrados=[]
-    beforeCoord = [0, 0]
-    for i in generados:
-        latitud = i.transform.location
-        latitud = map.transform_to_geolocation(carla.Location(x=latitud.x, y=latitud.y, z=0))
-        #if str(i.right_lane_marking.type)=="Broken" and str(i.left_lane_marking.type)=="Broken":
-        # if str(f.right_lane_marking.type) == "NONE" and str(f.left_lane_marking.type) == "NONE":
-        #if (i.is_junction and i.left_lane_marking.type != "NONE") or (str(i.right_lane_marking.type) == "Broken" and str(i.left_lane_marking.type) == "Broken"):
-        if i.is_junction and (str(i.right_lane_marking.type) != "NONE" and str(i.left_lane_marking.type) != "NONE") :
-            if latitud.longitude < -6.338950801925578 and latitud.longitude > -6.344916034757305 and latitud.latitude < 39.48059130922252 and latitud.latitude > 39.47907173926249:
-                if distanceBetweenCoords(beforeCoord[0], beforeCoord[1], latitud.latitude, latitud.longitude) > 10 :
-                    #print("Lat: " + str(latitud.latitude) + " Lon: " + str(latitud.longitude))
-                    #print("X: " + str(i.transform.location.x) + " Y: " + str(i.transform.location.y))
-                    x.append(i.transform.location.x)
-                    y.append(i.transform.location.y)
-                    filtrados.append(i)
-                    outputfileXY.write(str(i.transform.location.x) + "#" + str(i.transform.location.y) + "\n")
-                    outputfileLatLon.write(str(latitud.latitude) + "#" + str(latitud.longitude) + "\n")
-                    #print(i.left_lane_marking.type)
-                    #print(i.right_lane_marking.type)
-                    beforeCoord = [latitud.latitude, latitud.longitude]
-    outputfileXY.close()
-    outputfileLatLon.close
-
-    #plt.plot(x, y, 'ro')
-    #plt.show()
-
-
-    for index in range(len(x)):
-        pass #print("point: [",x[index],",",y[index], "]\n")
-    
-    # world = client.get_world()
     current_map = world.get_map()
     actors = world.get_actors()
 
@@ -131,7 +90,7 @@ try:
 
     print(vehicle.get_location())
 
-    vehicle.set_autopilot(True)
+    #vehicle.set_autopilot(True)
     #time.sleep(100)
     #vehicle.apply_control(carla.VehicleControl(throttle=-0, steer=0))
 
